cb209fda7087b9322dfb5b16ff0317a191d1bd81
simon <gatheru.simon432@gmail.com>
2019-06-18 16:00:51 +0300

    profile and assets models created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Assets(models.Model):
+    image = models.ImageField(upload_to='media/')
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
