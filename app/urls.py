ec4c0f6feac4f14b5ead4e26f82eca5db881dc93
simon <gatheru.simon432@gmail.com>
2019-06-18 17:07:23 +0300

    url for media defined and configured in settings

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.url.static import static
 
 urlpatterns = [
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
